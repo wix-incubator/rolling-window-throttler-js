2c9b34265a2f12a72ff938ca114dbfde44eb94e4
kfirb <kfirb@wix.com>
2016-12-08 15:35:43 +0200

    init project

---
+++ test/rolling-window-throttler.spec.js
@@ -0,0 +1,10 @@
+'use strict';
+const expect = require('chai').expect;
+  //build = require('..').get();
+
+describe('rolling window throttler', () => {
+  it('should create empty aspect given no matching cookies were provided', () => {
+  	expect(1).to.equal(1); 
+  });
+
+});
