fdc1af5dcc060feff0714cc1125a68421ce8d7a1
kfirb <kfirb@wix.com>
2016-12-08 16:22:26 +0200

    added 2 tests

--- test/rolling-window-throttler.spec.js
+++ test/rolling-window-throttler.spec.js
@@ -1,10 +1,24 @@
 'use strict';
-const expect = require('chai').expect;
-  //build = require('..').get();
+const expect = require('chai').expect,
+  factory = require('..');
 
 describe('rolling window throttler', () => {
-  it('should create empty aspect given no matching cookies were provided', () => {
-  	expect(1).to.equal(1); 
+
+  const key = '192.168.2.1';
+  const aDefaultThrottler = () => factory.get({max: 1, durationWindow: 1000});
+
+  beforeEach(function(){
+    this.throttler = aDefaultThrottler();
   });
+
+  it('Allow single request', function()  {
+    expect(this.throttler.tryAcquire(key)).to.be.true;
+  });
+
+  it('Throttle second request', function()  {
+    this.throttler.tryAcquire(key);
+    expect(this.throttler.tryAcquire(key)).to.be.false;
+  });
+
 
 });
