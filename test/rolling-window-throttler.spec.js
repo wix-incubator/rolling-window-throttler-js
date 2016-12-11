810ed8fe19ab64e13670185f611c008feae15ef0
Vilius Lukosius <vilius@wix.com>
2016-12-11 13:53:10 +0200

    Formatting and dropping -js postfix

--- test/rolling-window-throttler.spec.js
+++ test/rolling-window-throttler.spec.js
@@ -1,41 +1,42 @@
-'use strict';
 const expect = require('chai').expect,
   factory = require('..');
 
 describe('rolling window throttler', () => {
 
   const key = '192.168.2.1';
-  const aThrottler = (clock, durationWindow) => factory.get({max: 1,
-                                               durationWindow: durationWindow,
-                                               clock: clock});
+  const aThrottler = (clock, durationWindow) => factory.get({
+    max: 1,
+    durationWindow: durationWindow,
+    clock: clock
+  });
 
-  beforeEach(function(){
+  beforeEach(function () {
     this.clock = new FakeClock();
     this.throttler = aThrottler(this.clock, 1000);
   });
 
 
-  it('Should allow single request', function()  {
+  it('Should allow single request', function () {
     expect(this.throttler.tryAcquire(key)).to.be.true;
   });
 
-  it('Should throttle second request', function()  {
+  it('Should throttle second request', function () {
     this.throttler.tryAcquire(key);
     expect(this.throttler.tryAcquire(key)).to.be.false;
   });
 
-  it('should allow second request but from different key', function()  {
+  it('should allow second request but from different key', function () {
     const anotherKey = '200.200.200.1';
     expect(this.throttler.tryAcquire(key)).to.be.true;
     expect(this.throttler.tryAcquire(anotherKey)).to.be.true;
   });
 
-  it('should re-allow request after the rolling window', function()  {
+  it('should re-allow request after the rolling window', function () {
     this.throttler.tryAcquire(key);
     this.clock.age(2000);
     expect(this.throttler.tryAcquire(key)).to.be.true;
   });
-  it('should work with verbal durationWindow and not milliseconds', function()  {
+  it('should work with verbal durationWindow and not milliseconds', function () {
     const throttler = aThrottler(this.clock, '1s');
     throttler.tryAcquire(key);
     this.clock.age(2000);
@@ -43,21 +44,23 @@ describe('rolling window throttler', () => {
   });
 
 
-  it('Not providing clock should work, has default clock', function(){
+  it('Not providing clock should work, has default clock', function () {
     const throttler = aThrottler(null, 1000);
     expect(throttler.tryAcquire(key)).to.be.true;
   });
 
 });
 
-class FakeClock{
-  constructor(){
+class FakeClock {
+  constructor() {
     this.currentTime = new Date().getTime();
   }
-  age(ms){
+
+  age(ms) {
     this.currentTime = this.currentTime + ms;
   }
-  currentMillis(){
+
+  currentMillis() {
     return this.currentTime;
   }
 }
