fd4efef0158ff697f41ae53ceafd3d664302978b
Vilius Lukosius <vilius@wix.com>
2016-12-13 14:59:36 +0200

    better intellij formatting

--- test/rolling-window-throttler.spec.js
+++ test/rolling-window-throttler.spec.js
@@ -10,31 +10,31 @@ describe('rolling window throttler', () => {
   beforeEach(() => throttler = aThrottler(1000));
 
   it('Should allow single request', () => {
-    expect(throttler.tryAcquire(key)).to.be.true;
+    expect(throttler.tryAcquire(key)).to.equal(true);
   });
 
   it('Should throttle second request', () => {
     throttler.tryAcquire(key);
-    expect(throttler.tryAcquire(key)).to.be.false;
+    expect(throttler.tryAcquire(key)).to.equal(false);
   });
 
   it('should allow second request but from different key', () => {
     const anotherKey = '200.200.200.1';
-    expect(throttler.tryAcquire(key)).to.be.true;
-    expect(throttler.tryAcquire(anotherKey)).to.be.true;
+    expect(throttler.tryAcquire(key)).to.equal(true);
+    expect(throttler.tryAcquire(anotherKey)).to.equal(true);
   });
 
   it('should re-allow request after the rolling window', sinon.test(function() {
     throttler.tryAcquire(key);
     this.clock.tick(2000);
-    expect(throttler.tryAcquire(key)).to.be.true;
+    expect(throttler.tryAcquire(key)).to.equal(true);
   }));
 
   it('should work with verbal durationWindow and not milliseconds', sinon.test(function() {
     throttler = aThrottler('1s');
     throttler.tryAcquire(key);
     this.clock.tick(2000);
-    expect(throttler.tryAcquire(key)).to.be.true;
+    expect(throttler.tryAcquire(key)).to.equal(true);
   }));
 
   function aThrottler(durationWindow) {
